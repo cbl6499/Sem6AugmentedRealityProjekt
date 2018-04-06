2696033618b24e3bfb6798dcf2f195b7b7a69f58
dhu92 <dhutter92@gmail.com>
2018-04-06 11:51:56 +0200

    script
    player script

---
+++ Who am I/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private string username;
+    private int number;
+
+    public Player(string username, int number) {
+        this.Username = username;
+        this.Number = number;
+    }
+
+    public string Username{ get; set; }
+
+    public int Number { get; set; }
+
+    // Use this for initialization
+    void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
