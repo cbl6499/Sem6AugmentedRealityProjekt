203633680e4c5111473d77a9899ba6f539608285
ChristophRheinberger <crh6731@students.fhv.at>
2018-04-06 12:57:53 +0200

    GameMenu
    GameMenu v1 added

---
+++ Who am I/Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance { set; get; }
+
+    public GameObject mainMenu;
+    public GameObject serverMenu;
+    public GameObject connectMenu;
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+        serverMenu.SetActive(false);
+        connectMenu.SetActive(false);
+        DontDestroyOnLoad(gameObject);
+	}
+
+    public void ConnectButton() {
+        mainMenu.SetActive(false);
+        connectMenu.SetActive(true);
+    }
+
+    public void HostButton() {
+        mainMenu.SetActive(false);
+        serverMenu.SetActive(true);
+    }
+
+    public void ConnectToHostButton() {
+
+    }
+	
+    public void BackButton() {
+        serverMenu.SetActive(false);
+        connectMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
