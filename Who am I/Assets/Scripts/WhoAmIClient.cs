8b5d0584cb057ca9987d14bb44d5d62db18e29b7
Nepomuk1999 <christoph.bauer@students.fhv.at>
2018-04-24 08:17:43 +0200

    lo

--- Who am I/Assets/Scripts/WhoAmIClient.cs
+++ Who am I/Assets/Scripts/WhoAmIClient.cs
@@ -41,15 +41,12 @@ public class WhoAmIClient : NetworkBehaviour {
         this.MyClient.RegisterHandler(MsgType.Ready, OnReady);
         this.MyClient.RegisterHandler(MsgType.NotReady, PrintPlayerList);
         this.MyClient.RegisterHandler(MsgType.Connect, OnSuccessfulConnection);
-<<<<<<< HEAD
 
         
         this.MyClient.RegisterHandler(MsgType.UpdateVars, GameWon);
         
         // Debug.Log("Register PrintPlayerList");
         //Debug.Log(this.HostAddress);
-=======
->>>>>>> 220dd66f7dec4fba4ac4a70dcfb92e6fcf04a331
 
         this.Connect();
     }
@@ -57,12 +54,8 @@ public class WhoAmIClient : NetworkBehaviour {
     private void OnSuccessfulConnection(NetworkMessage netMsg) {
         MessageBase msgBase = netMsg.ReadMessage<Notification>();
         Debug.Log("hell yeah " + netMsg.conn.address);
-<<<<<<< HEAD
-        //throw new NotImplementedException();
 
-=======
         this.SendLobbyRegistration();
->>>>>>> 220dd66f7dec4fba4ac4a70dcfb92e6fcf04a331
     }
 
     public void Connect() {
@@ -92,7 +85,6 @@ public class WhoAmIClient : NetworkBehaviour {
         return new Notification(username);
     }
 
-    private void SendGuess(string guess)
     {
         Notification msg = new Notification();
         msg.Message = guess;
