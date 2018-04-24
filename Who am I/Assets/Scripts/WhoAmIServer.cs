08d1e0fc1f0c7c5102924ba6b2196447a3878d7d
Nepomuk1999 <christoph.bauer@students.fhv.at>
2018-04-24 08:50:55 +0200

    Revert "ko"
    This reverts commit e1a535a5ac9fb336351a8b2659aa16e9ed6696d7.

--- Who am I/Assets/Scripts/WhoAmIServer.cs
+++ Who am I/Assets/Scripts/WhoAmIServer.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.Networking.NetworkSystem;
 
 public class WhoAmIServer: NetworkBehaviour {
 
@@ -27,14 +26,13 @@ public class WhoAmIServer: NetworkBehaviour {
     public NetworkServer Server{ get; set; }
     public int Port { get; set; }
     public string HostAddress{ get; set; }
-
     public void SetupHost() {
         NetworkServer.Reset();
         this.Port = 63210;
         this.HostAddress = Network.player.ipAddress;
         NetworkServer.Listen(this.Port);
         GameLobby lobby = GameLobby.Instance;
-        lobby.SetOwner(Network.player.ipAddress);
+        lobby.SetOwner("Diego1337", "127.0.0.1");
         NetworkServer.RegisterHandler(MsgType.Connect, OnConnected);
         NetworkServer.RegisterHandler(MsgType.AddPlayer, ConnectToLobby);
 
@@ -43,11 +41,10 @@ public class WhoAmIServer: NetworkBehaviour {
     }
 
     public void ConnectToLobby(NetworkMessage netMsg) {
+        Notification msg = netMsg.ReadMessage<Notification>();
         GameLobby lobby = GameLobby.Instance;
-        Notification notification = new Notification();
-        notification.Deserialize(netMsg.reader);
-        Debug.Log(netMsg.ReadMessage<Notification>());
-        Debug.Log(notification.Message);
+        Debug.Log("Test");
+        lobby.RegisterPlayer(msg.Message, msg.Ip);
         if(lobby.Size == lobby.Players.Count) {
             BroadCastReady();
         } else {
@@ -65,33 +62,43 @@ public class WhoAmIServer: NetworkBehaviour {
         string players = "";
         GameLobby lobby = GameLobby.Instance;
         List<Player> playerList = lobby.Players;
+        foreach(Player p in playerList) {
+            players += p.Username + ",";
+        }
         BroadCastMessage(MsgType.SyncList, players);
     }
 
     private void BroadCastMessage(short type, string message) {
-        NetworkServer.SendToAll(type, new Notification(message));
+        GameLobby lobby = GameLobby.Instance;
+        List<Player> playerList = lobby.Players;
+        Notification msg = new Notification();
+        msg.Message = message;
+        NetworkServer.SendToAll(type, msg);
     }
 
     private void SendMessageToClient(NetworkMessage netMsg, short type, string text) {
         Notification msg = netMsg.ReadMessage<Notification>();
-        NetworkServer.SendToClient(netMsg.conn.connectionId, type, new Notification(text));
+        Notification answer = new Notification();
+        answer.Message = text;
+        answer.Ip = msg.Ip;
+        NetworkServer.SendToClient(netMsg.conn.connectionId, type, answer);
     }
 
     private void OnConnected(NetworkMessage netMsg) {
-        Debug.Log("Player connected server!");
         SendMessageToClient(netMsg, MsgType.Connect, "Success");
+        //throw new NotImplementedException();
     }
 
     public void StartGame() {
         BroadCastMessage(MsgType.LobbySceneLoaded, "Start");
     }
 
-
+<<<<<<< HEAD
     private void CheckGuess(NetworkMessage netMsg)
     {
         Notification msg = netMsg.ReadMessage<Notification>();
         GameLobby gl = GameLobby.Instance;
-        Boolean guessResult = gl.CheckGuess(msg.ToString(), msg.ToString());
+        Boolean guessResult = gl.CheckGuess(msg.ToString(), msg.Ip);
         Notification answer = new Notification();
         answer.Message = guessResult.ToString();
         NetworkServer.SendToClient(netMsg.conn.connectionId, MsgType.UpdateVars ,answer);
@@ -109,5 +116,6 @@ public class WhoAmIServer: NetworkBehaviour {
 	}
 
 
-
+=======
+>>>>>>> 220dd66f7dec4fba4ac4a70dcfb92e6fcf04a331
 }
