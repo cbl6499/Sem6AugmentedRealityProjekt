019176aada2e283e6b61b834354af7adc0c5201e
dhu92 <dhutter92@gmail.com>
2018-04-27 10:58:45 +0200

    fuck you

--- Who am I/Assets/Scripts/WhoAmIServer.cs
+++ Who am I/Assets/Scripts/WhoAmIServer.cs
@@ -36,7 +36,6 @@ public class WhoAmIServer: NetworkBehaviour {
         NetworkServer.RegisterHandler(MsgType.AddPlayer, ConnectToLobby);
         NetworkServer.RegisterHandler(MsgType.Owner, CreateLobby);
         NetworkServer.RegisterHandler(MsgType.UpdateVars, BroadCastPlayerFinished);
-        NetworkServer.RegisterHandler(MsgType.UpdateVars, CheckGuess);
     }
 
     public void CreateLobby(NetworkMessage netMsg) {
@@ -49,9 +48,10 @@ public class WhoAmIServer: NetworkBehaviour {
     public void ConnectToLobby(NetworkMessage netMsg) {
         StringMessage msg = netMsg.ReadMessage<StringMessage>();
         GameLobby lobby = GameLobby.Instance;
-        Debug.Log("Test");
-        lobby.RegisterPlayer(msg.value);
-        if(lobby.Size == lobby.Players.Count) {
+        Player player = lobby.CreatePlayer(msg.value);
+        lobby.RegisterPlayer(player);
+        SendMessageToClient(netMsg, MsgType.SpawnFinished, player.Number + "");
+        if (lobby.Size == lobby.Players.Count) {
             BroadCastReady();
         } else {
             BroadCastConnectedPlayer();
@@ -69,7 +69,7 @@ public class WhoAmIServer: NetworkBehaviour {
         GameLobby lobby = GameLobby.Instance;
         List<Player> playerList = lobby.Players;
         foreach(Player p in playerList) {
-            players += p.Username + ",";
+            players += p.Number + "|" + p.Username + ",";
         }
         BroadCastMessage(MsgType.SyncList, players);
     }
@@ -91,6 +91,12 @@ public class WhoAmIServer: NetworkBehaviour {
 
     public void StartGame() {
         BroadCastMessage(MsgType.LobbySceneLoaded, "Start");
+    }
+
+    public void BroadCastPlayerFinished(NetworkMessage netMsg) {
+        StringMessage message = netMsg.ReadMessage<StringMessage>();
+        int id = Int32.Parse(message.value);
+        
     }
 
 }
