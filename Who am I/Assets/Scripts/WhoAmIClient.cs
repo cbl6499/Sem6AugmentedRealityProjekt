5a3957de8063f2d1645295391617c2347ea03774
ChristophRheinberger <crh6731@students.fhv.at>
2018-04-27 10:42:04 +0200

    quickmerge

--- Who am I/Assets/Scripts/WhoAmIClient.cs
+++ Who am I/Assets/Scripts/WhoAmIClient.cs
@@ -35,16 +35,16 @@ public class WhoAmIClient : NetworkBehaviour {
     public void SetupClient() {
         this.MyClient = new NetworkClient();
         this.MyClient.RegisterHandler(MsgType.Ready, OnReady);
-        this.MyClient.RegisterHandler(MsgType.NotReady, PrintPlayerList);
+        this.MyClient.RegisterHandler(MsgType.SyncList, SetPlayerList);
         this.MyClient.RegisterHandler(MsgType.Connect, OnSuccessfulConnection);
-        
+        this.MyClient.RegisterHandler(MsgType.SpawnFinished, SetLocalClient);
         this.MyClient.RegisterHandler(MsgType.UpdateVars, GameWon);
         
         this.Connect();
     }
 
     private void OnSuccessfulConnection(NetworkMessage netMsg) {
-        StringMessage msgBase = netMsg.ReadMessage<StringMessage>();
+        StringMessage msgBase = netMsg.ReadMessage<StringMessage>();        
         Debug.Log("hell yeah " + netMsg.conn.address);
         this.SendLobbyRegistration();
     }
@@ -67,9 +67,19 @@ public class WhoAmIClient : NetworkBehaviour {
         Debug.Log("Yay, everyone is ready");
     }
 
-    private void PrintPlayerList(NetworkMessage netMsg) {
+    private void SetLocalClient(NetworkMessage netMsg) {
+        StringMessage msg = netMsg.ReadMessage<StringMessage>();
+        this.localClient = Int32.Parse(msg.value);
+    }
+
+    private void SetPlayerList(NetworkMessage netMsg) {
         StringMessage msg = netMsg.ReadMessage<StringMessage>();
         Debug.Log("Yay, " + msg.value + " are in the lobby");
+        string[] array = msg.value.Split(',');
+        foreach(string a in array) {
+            string[] player = a.Split('|');
+            playerList.Add(new Player(Int32.Parse(player[0]), player[1]));
+        }
     }
 
     private StringMessage CreateConnectionMessage(string username, string userIp) {
@@ -78,7 +88,7 @@ public class WhoAmIClient : NetworkBehaviour {
 
     private void SendCorrectGuess()
     {
-        myClient.Send(MsgType.UpdateVars, new StringMessage("Success"));
+        myClient.Send(MsgType.UpdateVars, new StringMessage(localClient.ToString()));
     }
 
     private void GameWon(NetworkMessage netMsg)
