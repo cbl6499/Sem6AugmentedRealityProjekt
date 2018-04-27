2349e4f3b79041a17158f2a9b1d0165dec6aa7ed
dhu92 <dhutter92@gmail.com>
2018-04-27 09:29:39 +0200

    player changes

--- Who am I/Assets/Scripts/Player.cs
+++ Who am I/Assets/Scripts/Player.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player {// : MonoBehaviour {
+public class Player {
 
     private int number;
-    //private string ip;
     private string username;
     private string face;
 
@@ -17,28 +16,6 @@ public class Player {// : MonoBehaviour {
     public string Username { get; set; }
     public int Number { get; set; }
 
-    public string Ip { get; set; }
+    public string Face { get; set; }
 
-    public string Face
-    {
-        get
-        {
-            return face;
-        }
-
-        set
-        {
-            face = value;
-        }
-    }
-
-    // Use this for initialization
-    void Start () {
-	    	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
