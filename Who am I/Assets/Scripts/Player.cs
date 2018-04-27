fccd3959cf1efdee92772938c96ce0e7b8bbebb0
dhu92 <dhutter92@gmail.com>
2018-04-27 10:02:48 +0200

    player points added

--- Who am I/Assets/Scripts/Player.cs
+++ Who am I/Assets/Scripts/Player.cs
@@ -7,12 +7,18 @@ public class Player {
     private int number;
     private string username;
     private string face;
+    private int points;
 
     public Player(int number, string username) {
         this.Number = number;
         this.Username = username;
     }
 
+    public void AddPoints(int points) {
+        points += points;
+    }
+
+    public int Points { get; set; }
     public string Username { get; set; }
     public int Number { get; set; }
 
